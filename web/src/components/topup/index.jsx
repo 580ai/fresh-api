5eb696c450b5ac5f8904ecfbda8492da0885c662
LittleROCK <2472100909@qq.com>
2026-04-14 15:45:17 +0800

    feat: 去掉邀请返现功能

--- web/src/components/topup/index.jsx
+++ web/src/components/topup/index.jsx
@@ -776,14 +776,14 @@ const TopUp = () => {
           allSubscriptions={allSubscriptions}
           reloadSubscriptionSelf={getSubscriptionSelf}
         />
-        <InvitationCard
-          t={t}
-          userState={userState}
-          renderQuota={renderQuota}
-          setOpenTransfer={setOpenTransfer}
-          affLink={affLink}
-          handleAffLinkClick={handleAffLinkClick}
-        />
+        {/*<InvitationCard*/}
+        {/*  t={t}*/}
+        {/*  userState={userState}*/}
+        {/*  renderQuota={renderQuota}*/}
+        {/*  setOpenTransfer={setOpenTransfer}*/}
+        {/*  affLink={affLink}*/}
+        {/*  handleAffLinkClick={handleAffLinkClick}*/}
+        {/*/>*/}
       </div>
     </div>
   );
